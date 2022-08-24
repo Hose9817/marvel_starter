b7a3e884edd52309fdd62fa35618a5dccf65b26e
Hose9817 <abdullayev6859@gmail.com>
2022-08-24 23:23:30 +0600

    added functional with getting random character from marvel-server

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,10 +1,10 @@
 
 
 class MarvelService {
-    
+
     _apiBase = 'https://gateway.marvel.com:443/v1/public/';
     _apiKey = process.env.REACT_APP_MARVEL_API_KEY;
-    
+
 
     getResource = async (url) => {
         let res = await fetch(url);
@@ -16,11 +16,23 @@ class MarvelService {
         return await res.json();
     }
 
-    getAllCaracters = () => {
-        return this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+    getAllCaracters = async () => {
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+        return res.data.results.map(this._transformCharacter)
     }
-    getCaracter = (id) => {
-        return this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+    getCharacter = async (id) => {
+        const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+        return this._transformCharacter(res.data.results[0]);
+    }
+
+    _transformCharacter = (char) => {
+        return {
+            name: char.name,
+            description: char.description,
+            thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url
+        }
     }
 
 }
