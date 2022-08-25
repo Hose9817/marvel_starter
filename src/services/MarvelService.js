a966a21f568c20336c5725dc430df00070a506bd
Hose9817 <abdullayev6859@gmail.com>
2022-08-25 11:12:17 +0600

    added error-gif and spiner for randomCharacter

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -28,7 +28,7 @@ class MarvelService {
     _transformCharacter = (char) => {
         return {
             name: char.name,
-            description: char.description ? char.description.slice(0, 100) + '...' : 'Not have descr!',
+            description: char.description ? `${char.description.slice(0, 100)}...` : 'There is no description for this character',
             thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
             homepage: char.urls[0].url,
             wiki: char.urls[1].url
